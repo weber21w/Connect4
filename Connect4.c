3b5ae3085458c0f5bdd2cc6b63e4cf93afb0913b
Lee Weber <weber21w@gmail.com>
2025-12-06 22:55:42 -0600

    Update Connect4.c
    bug fixes

--- Connect4.c
+++ Connect4.c
@@ -1,6 +1,6 @@
 /*
- *  Dr.Mario port for Uzebox ver 1.1
- *  Copyright (C) 2009  Codecrank, Mapes
+ *  Connect4 FujiNet(via uzenet-virtual-fujinet) Client
+ *  Copyright (C) 2026  D3thAdd3r (Lee Weber)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -38,10 +38,10 @@ extern volatile u8 uzenet_error;
 
 uint16_t pads[2],oldPads[2];
 
-#define BOARD_WIDTH_STD		7//size of original game board...
+#define BOARD_WIDTH_STD		7	//size of original game board...
 #define BOARD_HEIGHT_STD	6
 
-#define BOARD_WIDTH_EXT		8//for whatever reason, the existing ports use a larger board...
+#define BOARD_WIDTH_EXT		8	//for whatever reason, the existing FujiNet ports use a larger board...
 #define BOARD_HEIGHT_EXT	7
 
 uint8_t board[BOARD_WIDTH_EXT*BOARD_HEIGHT_EXT];
@@ -55,12 +55,218 @@ uint8_t frame;
 #define NET_ROLE_PLAYER2	2
 #define NET_ROLE_SPECTATOR	3
 
-#include "ai.h"
-
 char pname[2][13+1];
 uint8_t chat_column,chat_row;
 uint8_t mode, turn, winner, move;
 
+//AI Stuff
+uint8_t sim[BOARD_WIDTH_EXT*BOARD_HEIGHT_EXT];//AI simulation buffer
+uint8_t weights[BOARD_WIDTH_EXT];
+uint8_t levels[BOARD_WIDTH_EXT];
+uint8_t ai_target = 0;
+
+void db(){
+	TriggerFx(0,120,1);
+}
+
+void AIDrawSim(){
+	uint16_t goff;
+	for(uint8_t i=0;i<board_height;i++){
+		goff = i*BOARD_WIDTH_EXT;
+		for(uint8_t j=0;j<board_width;j++){
+			PrintInt(5+(j*5),3+(i*4),sim[goff++],1);
+		}
+	}
+}
+
+static uint8_t AI_CheckWinAt(uint8_t column, uint8_t row, uint8_t color){
+	uint8_t count;
+	uint8_t x,y;
+
+	//horizontal
+	count = 1;
+	x = column;
+	y = row;
+	//left
+	while(x>0){
+		x--;
+		if(sim[y*BOARD_WIDTH_EXT + x] != color) break;
+		count++;
+	}
+	//right
+	x = column;
+	while((x+1) < board_width){
+		x++;
+		if(sim[y*BOARD_WIDTH_EXT + x] != color) break;
+		count++;
+	}
+	if(count >= 4) return 1;
+
+	//vertical
+	count = 1;
+	x = column;
+	y = row;
+	//up
+	while(y>0){
+		y--;
+		if(sim[y*BOARD_WIDTH_EXT + x] != color) break;
+		count++;
+	}
+	//down
+	y = row;
+	while((y+1) < board_height){
+		y++;
+		if(sim[y*BOARD_WIDTH_EXT + x] != color) break;
+		count++;
+	}
+	if(count >= 4) return 1;
+
+	//diagonal: up-left / down-right
+	count = 1;
+	x = column;
+	y = row;
+	while(x>0 && y>0){
+		x--;
+		y--;
+		if(sim[y*BOARD_WIDTH_EXT + x] != color) break;
+		count++;
+	}
+	x = column;
+	y = row;
+	while((x+1) < board_width && (y+1) < board_height){
+		x++;
+		y++;
+		if(sim[y*BOARD_WIDTH_EXT + x] != color) break;
+		count++;
+	}
+	if(count >= 4) return 1;
+
+	//diagonal: up-right / down-left
+	count = 1;
+	x = column;
+	y = row;
+	while((x+1) < board_width && y>0){
+		x++;
+		y--;
+		if(sim[y*BOARD_WIDTH_EXT + x] != color) break;
+		count++;
+	}
+	x = column;
+	y = row;
+	while(x>0 && (y+1) < board_height){
+		x--;
+		y++;
+		if(sim[y*BOARD_WIDTH_EXT + x] != color) break;
+		count++;
+	}
+	if(count >= 4) return 1;
+
+	return 0;
+}
+
+uint8_t AIFindWinningMove(uint8_t color){//returns column of immediate winning move for given color, or 255 if none
+	for(uint8_t col=0;col<board_width;col++){
+		//full column?
+		if(sim[col])
+			continue;
+
+		uint8_t row;
+		for(row=0;row<board_height;row++){
+			if(sim[(row*BOARD_WIDTH_EXT)+col])
+				break;
+		}
+
+		uint8_t drop_row;
+		if(row == board_height){
+			drop_row = board_height-1;
+		}else{
+			drop_row = row-1;
+		}
+
+		uint16_t idx = (drop_row*BOARD_WIDTH_EXT) + col;
+		sim[idx] = color;
+		if(AI_CheckWinAt(col,drop_row,color)){
+			sim[idx] = 0;
+			return col;
+		}
+		sim[idx] = 0;
+	}
+	return 255;
+}
+
+uint8_t AIThink(){//this is called with the assumption the move to be played is the AI player
+	uint8_t i,j,color;
+	uint16_t c;
+	uint16_t loff;
+	uint8_t mag,best;
+	uint8_t opponent;
+
+	color = 0;
+	for(c=0;c<BOARD_WIDTH_EXT*BOARD_HEIGHT_EXT;c++){//determine which color is to play next(and assume we are that)
+		if(board[c])
+			color++;
+		sim[c] = board[c];//copy the board to the AI buffer also...
+	}
+
+	color &= 1;//if an odd number, the color to play is yellow, otherwise red
+	color += 1;//in the actual board data, 0 is empty, 1 is red, 2 is yellow
+	if(color == 2)
+		opponent = 1;
+	else
+		opponent = 2;
+
+	//first, see if there is an immediate win option
+	i = AIFindWinningMove(color);
+	if(i != 255){
+		db();
+		return i;
+	}
+
+	for(i=0;i<board_width;i++){//get the height of each column based on previously dropped pieces
+		for(j=0;j<board_height;j++){
+			if(board[(j*BOARD_WIDTH_EXT)+i])
+				break;
+		}
+		levels[i] = j;
+		//all else equal, move to columns with more free spaces(buy more time for the opponent to make an error...)
+		weights[i] = (10*j);
+	}
+
+	for(i=0;i<board_width;i++){//try every valid move, and see if it leads to immediate defeat
+		if(sim[i])//can't play this column
+			continue;
+		loff = ((levels[i]-1)*BOARD_WIDTH_EXT)+i;
+		sim[loff] = color;
+		levels[i]--;
+
+		uint8_t m = AIFindWinningMove(opponent);//see if there is a winning move for the opponent after this simulated move
+
+		levels[i]++;
+		sim[loff] = 0;
+		if(m < 255){
+			weights[i] = 0;
+		}
+	}
+
+	mag = 0;
+	best = 3;
+	for(i=0;i<board_width;i++){
+		if(weights[i] > mag){//current best move?
+			mag = weights[i];
+			best = i;
+		}
+	}
+	if(mag == 0){//no move was good? we must be forced to play a losing move...
+		for(i=0;i<board_width;i++){//just select the first legal move then...
+			if(!board[i]){
+				best = i;
+				break;
+			}
+		}
+	}
+
+	return best;
+}
 
 void Intro(){
 //return;
@@ -91,10 +297,9 @@ void Intro(){
 			SetTile(j,i,1);
 		for(uint8_t j=0;j<SCREEN_TILES_H;j++){
 			for(uint8_t k=11;k<18;k++)
-			SetTile(j,k,0);
+				SetTile(j,k,0);
 		}
-		//if(i <= 13){
-			Print(15,13,PSTR("U Z E B O X"));
+		Print(15,13,PSTR("U Z E B O X"));
 
 		if(i <= 17){
 			uint8_t detected = 0;
@@ -149,8 +354,6 @@ uint8_t UzenetScreen(){
 		TriggerFx(6,0xff,1);
 	}
 
-//	if(1)
-//		Print(0,20,PSTR(" Uzenet Module Detected"));
 	Print(0,21,PSTR(" Connecting to Uzenet..."));
 	while(1){
 		WaitVsync(1);
@@ -161,7 +364,6 @@ uint8_t UzenetScreen(){
 uint8_t TitleScreen(){
 	SetTileTable(title_data);
 	ClearVram();
-//	for(uint8_t i=0;i<SCREEN_TILES_H;i++){SetTile(i,SCREEN_TILES_V,0);SetTile(i,SCREEN_TILES_V+1,0);} //hmm...
 	DrawMap(0,0,title_map);
 	Print(6,21,PSTR("                  "));
 	Print(6,22,PSTR("  1 Player Game   "));
@@ -174,7 +376,6 @@ uint8_t TitleScreen(){
 	while(1){
 		WaitVsync(1);
 		GetPrngNumber(0);
-//pads[0] = BTN_START;
 		if(pads[0] & BTN_START && !(oldPads[0] & BTN_START))
 			break;
 		if(pads[0] & BTN_UP && !(oldPads[0] & BTN_UP) && cursor)
@@ -192,15 +393,13 @@ uint8_t TitleScreen(){
 	return cursor;
 }
 
-uint8_t PrintByteVertical(uint8_t x, uint8_t y, uint8_t b){
-
+void PrintByteVertical(uint8_t x, uint8_t y, uint8_t b){
 	for(uint8_t i=0;i<2;i++){
 		uint8_t c=b%10;
 		if(b>0 || i==0){
 			SetFont(x,y--,c+'0'-32);
 		}else{
-				SetFont(x,y--,'0'-32);
-
+			SetFont(x,y--,'0'-32);
 		}
 		b=b/10;
 	}
@@ -217,7 +416,6 @@ uint8_t PrintRamVertical(uint8_t x, uint8_t y, unsigned char *string){
 		}else
 			return y;
 	}
-	
 }
 
 uint8_t PrintRamHorizontal(uint8_t x, uint8_t y, unsigned char *string){
@@ -231,7 +429,6 @@ uint8_t PrintRamHorizontal(uint8_t x, uint8_t y, unsigned char *string){
 		}else
 			return x;
 	}
-	
 }
 
 uint8_t PrintVertical(uint8_t x, uint8_t y, const char *string){
@@ -246,14 +443,13 @@ uint8_t PrintVertical(uint8_t x, uint8_t y, const char *string){
 			return y;
 		}
 	}
-	
 }
 
 void DrawWins(){
-	PrintRamVertical(1,3,pname[0]);
+	PrintRamVertical(1,3,(unsigned char*)pname[0]);
 	PrintVertical(1,17,PSTR("WINS"));
 	PrintByteVertical(1,23,wins[0]);
-	PrintRamVertical(SCREEN_TILES_H-2,3,pname[1]);
+	PrintRamVertical(SCREEN_TILES_H-2,3,(unsigned char*)pname[1]);
 	PrintVertical(SCREEN_TILES_H-2,17,PSTR("WINS"));
 	PrintByteVertical(SCREEN_TILES_H-2,23,wins[1]);
 }
@@ -268,7 +464,6 @@ void DrawBoard(){
 
 	DrawWins();
 }
-
 
 uint8_t JoinGame(){
 	return 0;
@@ -290,9 +485,7 @@ void ChatPrint(const char *string){
 			break;
 		}
 	}
-	
 }
-
 
 void ClearBoard(){
 	for(int16_t i=0;i<BOARD_WIDTH_EXT*BOARD_HEIGHT_EXT;i++)
@@ -305,12 +498,10 @@ uint8_t GetBoard(uint8_t column, uint8_t row){
 
 void RedrawPosition(uint8_t column, uint8_t row){
 	uint8_t color = GetBoard(column,row);
-//	PrintInt(10,10,color,1);
-	if(board_width == BOARD_WIDTH_EXT){//extended size(compressed spacing)
-	
-	//	DrawMap(2+(column*4),0+(row*3),pgm_read_word(&piece_frames[3+color]));
-	}else{//standard size board(full spacing)
-	
+	if(board_width == BOARD_WIDTH_EXT){
+		//extended size(compressed spacing) - currently no per-cell redraw tiles defined
+	}else{
+		//standard size board(full spacing)
 		DrawMap(3+(column*5),1+(row*4),pgm_read_word(&piece_frames[4+color]));
 	}
 }
@@ -325,80 +516,114 @@ uint8_t DropPiece(uint8_t p, uint8_t column){
 	if(column >= BOARD_WIDTH_EXT)
 		column = BOARD_WIDTH_EXT-1;
 
-  	uint8_t row = 0;
-  	p++;//0 is blank, 1 is P1, 2 is P2
+	uint8_t row = 0;
+	p++;//0 is blank, 1 is P1, 2 is P2
 
 	while(!GetBoard(column,row)){//animate dropping piece until we hit something below
 		WaitVsync(2);
 
-	  	if(row == 0){
-	  //		TriggerFx(1,255,1);
-			
-		}else
-	  		SetBoard(column,row-1,0);//clear previous position(this will automatically redraw)
+		if(row == 0){
+			//TriggerFx(1,255,1);
+		}else{
+			SetBoard(column,row-1,0);//clear previous position(this will automatically redraw)
+		}
 
 		SetBoard(column,row++,p);
 		if(row == board_height)
 			break;
 	}
 	if(row == 0){//failed on first move?
-//		TriggerFx(4,255,1);
+		//TriggerFx(4,255,1);
 		return 254;//try again..
 	}
 	row--;
-//	TriggerFx(2,255,1);
+	//TriggerFx(2,255,1);
 
+	//Check for winning (check only lines passing through last move)
+	uint8_t count;
+	uint8_t x,y;
 
-	//Check for winning:
-	uint8_t run = 0;
-	for(uint8_t x=0;x<board_width;x++){//check horizontal
-		if(GetBoard(x,row) != p)
-			run = 0;
-		else if(++run > 3)
-			return p-1;
+	//horizontal
+	count = 1;
+	x = column;
+	y = row;
+	//left
+	while(x>0){
+		x--;
+		if(GetBoard(x,y) != p) break;
+		count++;
 	}
-
-	run = 0;
-	for(uint8_t y=0;y<board_height;y++){//check vertical
-		if(GetBoard(column,y) != p)
-			run = 0;
-		else if(++run > 3)
-			return p-1;
+	//right
+	x = column;
+	while((x+1) < board_width){
+		x++;
+		if(GetBoard(x,y) != p) break;
+		count++;
 	}
+	if(count >= 4)
+		return p-1;
 
-	uint8_t t1 = column;
-	uint8_t t2 = row;
-	while(t1 && t2){//get top left
-		t1--;
-		t2--;
+	//vertical
+	count = 1;
+	x = column;
+	y = row;
+	//up
+	while(y>0){
+		y--;
+		if(GetBoard(x,y) != p) break;
+		count++;
 	}
-
-	run = 0;
-	while(1){//scan from top left to bottom right
-
-		if(GetBoard(t1++,t2++) != p)
-			run = 0;
-		else if(++run > 3)
-			return p-1;
-		if(t1 == board_width || t2 == board_height)
-			break;
+	//down
+	y = row;
+	while((y+1) < board_height){
+		y++;
+		if(GetBoard(x,y) != p) break;
+		count++;
 	}
+	if(count >= 4)
+		return p-1;
 
-	while(column < board_width-1 && row){//get top right
-		column++;
-		row--;
+	//diagonal up-left / down-right
+	count = 1;
+	x = column;
+	y = row;
+	while(x>0 && y>0){
+		x--;
+		y--;
+		if(GetBoard(x,y) != p) break;
+		count++;
 	}
-
-	run = 0;
-	while(1){//scan from top right to bottom left
-
-		if(GetBoard(column--,row++) != p)
-			run = 0;
-		else if(++run > 3)
-			return p-1;
-		if(column == board_width || row == board_height)
-			break;
+	x = column;
+	y = row;
+	while((x+1) < board_width && (y+1) < board_height){
+		x++;
+		y++;
+		if(GetBoard(x,y) != p) break;
+		count++;
 	}
+	if(count >= 4)
+		return p-1;
+
+	//diagonal up-right / down-left
+	count = 1;
+	x = column;
+	y = row;
+	while((x+1) < board_width && y>0){
+		x++;
+		y--;
+		if(GetBoard(x,y) != p) break;
+		count++;
+	}
+	x = column;
+	y = row;
+	while(x>0 && (y+1) < board_height){
+		x--;
+		y++;
+		if(GetBoard(x,y) != p) break;
+		count++;
+	}
+	if(count >= 4)
+		return p-1;
   
 	return 255;//no victory, no error
 }
@@ -413,16 +638,15 @@ int main(){
 	SetMasterVolume(224);
 	Intro();
 
-  	uint8_t cursor=0;//the horizontal position above the board where a piece will drop from, controlled by the active player
+	uint8_t cursor=0;//the horizontal position above the board where a piece will drop from, controlled by the active player
 PROGRAM_TOP:
 	chat_column = chat_row = 0;
 	mode = TitleScreen();
 	if(mode == 2){
-		net_role == UzenetScreen();
+		net_role = UzenetScreen();
 		if(!net_role)
 			goto PROGRAM_TOP;
 		net_role = mode;
-
 
 		board_width = BOARD_WIDTH_EXT;//be compatible with existing clients using non-standard board size
 		board_height = BOARD_HEIGHT_EXT;
@@ -460,19 +684,16 @@ MOVE_TOP:
 	
 	if(turn){
 		if(frame&0b1000){
-
 			if(!board[cursor]){
 				SetTile(4+(cursor*5),1,49);
 				SetTile(5+(cursor*5),1,50);
 			}else{
 				SetTile(4+(cursor*5),1,53);
-				SetTile(4+(cursor*5),1,54);
+				SetTile(5+(cursor*5),1,54);
 			}
 		}
-		
 	}else{
 		if(frame&0b1000){
-
 			if(!board[cursor]){
 				SetTile(4+(cursor*5),1,47);
 				SetTile(5+(cursor*5),1,48);
@@ -482,47 +703,37 @@ MOVE_TOP:
 			}
 		}
 	}
-if(turn){
-	if(ai_target < 255){
-	//	PrintByte(30,25,ai_target,1);
-		if(cursor > ai_target)
-			cursor--;
-		else if(cursor < ai_target)
-			cursor++;
-		else{
-			pads[0] = BTN_B;oldPads[0] = 0;
-			ai_target = 255;	
-		}
-	}else{
-		ai_target = AIThink();
-	//	if(board[ai_target])
-	//		ai_target = 3;
-	//		db();
-//		AIDrawSim();
-	//	PrintByte(30,25,ai_target,1);
 
-//		for(uint8_t l=0;l<board_width;l++)
-//			PrintByte(5+(l*5),24,weights[l],1);
-//		goto MOVE_TOP;
-	//	continue;
-	}	
-}
+	if(turn){
+		if(ai_target < 255){
+			if(cursor > ai_target)
+				cursor--;
+			else if(cursor < ai_target)
+				cursor++;
+			else{
+				pads[0] = BTN_B;oldPads[0] = 0;
+				ai_target = 255;	
+			}
+		}else{
+			ai_target = AIThink();
+		}	
+	}
+
 	if(cursor && (pads[0] & BTN_LEFT) && !(oldPads[0] & BTN_LEFT))
 		cursor--;
 	if(cursor < board_width-1 && (pads[0] & BTN_RIGHT) && !(oldPads[0] & BTN_RIGHT))
 		cursor++;
 
-	if(((!(pads[0] & BTN_B)) || (oldPads[0] & BTN_B)))// || (!(pads[turn] & BTN_Y) || (oldPads[turn] & BTN_Y)))
+	if(((!(pads[0] & BTN_B)) || (oldPads[0] & BTN_B)))
 		goto MOVE_TOP;
 
 	winner = DropPiece(turn,cursor);
 
-//PrintByte(20,21,move,1);	
 	if(winner == 254)
 		goto MOVE_TOP;//invalid move, try again
 	if(winner < 3)
 		goto GAME_END;
-//MOVE_END:
+
 	turn = !turn;//otherwise, next player's turn
 	Print(3,25,turn?PSTR("P2's Turn"):PSTR("P1's Turn"));
 	move++;
@@ -534,7 +745,7 @@ GAME_END:
 		Print(10,25,PSTR("Draw Game"));
 	}else{
 		wins[winner]++;
-		PrintRam(10,25,pname[winner]);
+		PrintRamHorizontal(10,25,(unsigned char*)pname[winner]);
 		uint8_t x;
 		for(x=0;x<13;x++){
 			if(pname[winner][x] == '\0')
